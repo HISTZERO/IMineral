4f8b09d281db7e64674d2058a66991f428f30eb8
Tran Minh Tien <minhtien@vnpt.vn>
2020-09-10 10:54:31 +0700

    delete component k dùng

--- src/app/features/admin/thietlap/thietlap.module.ts
+++ src/app/features/admin/thietlap/thietlap.module.ts
@@ -3,19 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NgbAlertModule } from "@ng-bootstrap/ng-bootstrap";
 import { MatSidenavModule, MatSlideToggleModule } from "@angular/material";
 import { GridModule } from "@syncfusion/ej2-angular-grids";
+import { TranslateModule } from "@ngx-translate/core";
 
-import { ThietlapComponent } from './thietlap.component';
-import { ThietlaphethongListComponent } from './thietlaphethong/thietlaphethong-list/thietlaphethong-list.component';
-import { ThietlaphethongIoComponent } from './thietlaphethong/thietlaphethong-io/thietlaphethong-io.component';
+import { ThietlapComponent } from 'src/app/features/admin/thietlap/thietlap.component';
+import { ThietlaphethongListComponent } from 'src/app/features/admin/thietlap/thietlaphethong/thietlaphethong-list/thietlaphethong-list.component';
+import { ThietlaphethongIoComponent } from 'src/app/features/admin/thietlap/thietlaphethong/thietlaphethong-io/thietlaphethong-io.component';
 import { ThietlapRoutingModule } from "src/app/features/admin/thietlap/thietlap-routing.module";
 import { AdminSharedModule } from "src/app/features/admin/admin-shared.module";
-import { ThietlaptramListComponent } from './thietlaptram/thietlaptram-list/thietlaptram-list.component';
-import { ThietlaptramIoComponent } from './thietlaptram/thietlaptram-io/thietlaptram-io.component';
-import { DoituongthietlapListComponent } from './thietlaptram/doituongthietlap/doituongthietlap-list/doituongthietlap-list.component';
-import { DoituongthietlapIoComponent } from './thietlaptram/doituongthietlap/doituongthietlap-io/doituongthietlap-io.component';
-import { ThietlapdulieuListComponent } from './thietlapdulieu/thietlapdulieu-list/thietlapdulieu-list.component';
-import { ThietlapdulieuIoComponent } from './thietlapdulieu/thietlapdulieu-io/thietlapdulieu-io.component';
-import { TranslateModule } from "@ngx-translate/core";
 
 @NgModule({
   imports: [
@@ -28,10 +22,12 @@ import { TranslateModule } from "@ngx-translate/core";
     NgbAlertModule,
     TranslateModule
   ],
-  declarations: [ThietlapComponent, ThietlaphethongListComponent, ThietlaphethongIoComponent, ThietlaptramListComponent, ThietlaptramIoComponent, DoituongthietlapListComponent, DoituongthietlapIoComponent, ThietlapdulieuListComponent, ThietlapdulieuIoComponent],
-  entryComponents: [ThietlaphethongIoComponent,
-    ThietlaptramIoComponent,
-    DoituongthietlapIoComponent,
-    ThietlapdulieuIoComponent]
+  declarations: [
+    ThietlapComponent,
+    ThietlaphethongListComponent,
+    ThietlaphethongIoComponent],
+  entryComponents: [
+    ThietlaphethongIoComponent,
+    ]
 })
 export class ThietlapModule { }
