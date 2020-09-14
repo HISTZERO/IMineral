5f8c627f32378b2a0b2b936a489edc58fd4abfcd
Tran Minh Tien <minhtien@vnpt.vn>
2020-09-14 17:35:23 +0700

    list cauhinhtailieuio

--- src/app/features/admin/thietlap/cauhinhtailieu/cauhinhtailieu-io/cauhinhtailieu-io.component.ts
+++ src/app/features/admin/thietlap/cauhinhtailieu/cauhinhtailieu-io/cauhinhtailieu-io.component.ts
@@ -14,8 +14,8 @@ import { SettingsCommon, ThietLapHeThong } from "../../../../../shared/constants
 export class CauhinhtailieuIoComponent implements OnInit {
 
   // Viewchild template
-  @ViewChild('gridLoaiTaiLieu', { static: true }) public gridLoaiTaiLieu: GridComponent;
-  @ViewChild('gridCauHinhTaiLieu', { static: true }) public gridCauHinhTaiLieu: GridComponent;
+  @ViewChild('gridLoaiTaiLieu', { static: false }) public gridLoaiTaiLieu: GridComponent;
+  @ViewChild('gridCauHinhTaiLieu', { static: false }) public gridCauHinhTaiLieu: GridComponent;
 
   // Chứa thiết lập grid
   public settingsCommon = new SettingsCommon();
@@ -26,11 +26,24 @@ export class CauhinhtailieuIoComponent implements OnInit {
   // Chứa danh sách Loại tài liệu
   public listLoaitaiLieu: OutputDmLoaiTaiLieuModel[];
 
+  // Chứa danh sách cấu hình tài liệu
+  public listCauHinhTaiLieu: any = [];
+
+  // Chứa danh sách loại tài liệu đã chọn
+  public listLoaiTaiLieuSelected: any;
+
+  // Chứa danh sách cấu hình tài liệu đã chọn
+  public listCauHinhTaiLieuSelected: any;
+
   // Chứa dữ liệu đã chọn 
   public selectedItem: OutputDmLoaiTaiLieuModel;
 
   // Chứa dữ liệu translate
   public dataTranslate: any;
+
+  // Chứa thuộc tính disable button
+  public disableBtnRight: boolean = true;
+  public disableBtnLeft: boolean = true;
 
 
   constructor(
@@ -89,5 +102,45 @@ export class CauhinhtailieuIoComponent implements OnInit {
       });
     }
     this.listLoaitaiLieu = listData.items;
+  }
+
+  /**
+   * Hàm lấy dữ liệu khi click vào row trên grid Loại tài liệu
+   */
+  public rowSelectedLoaiTaiLieu() {
+    this.listLoaiTaiLieuSelected = this.gridLoaiTaiLieu.getSelectedRecords();
+  }
+
+  /**
+   * Hàm lấy dữ liệu khi click vào row trên grid cấu hình tài liệu
+   */
+  public rowSelectedCauHinhTaiLieu() {
+    this.listCauHinhTaiLieuSelected = this.gridCauHinhTaiLieu.getSelectedRecords();
+  }
+
+  /**
+   * Nút chuyển toàn bộ dữ liệu grid loại tài liệu đã chọn sang grid cấu hình tài liệu
+   */
+  public btnDoubleRight() {
+    this.listCauHinhTaiLieu = Array.prototype.concat.call(this.listLoaiTaiLieuSelected, this.gridCauHinhTaiLieu.dataSource);
+    for (let i = this.listLoaitaiLieu.length; i--;) {
+      if (this.listLoaiTaiLieuSelected.indexOf(this.listLoaitaiLieu[i]) !== -1) {
+        this.listLoaitaiLieu.splice(i, 1);
+      }
+    }
+    this.gridLoaiTaiLieu.refresh();
+  }
+
+  /**
+   * Nút chuyển toàn bộ dữ liệu grid loại tài liệu đã chọn sang grid cấu hình tài liệu
+   */
+  public btnDoubleLeft() {
+    this.listLoaitaiLieu = Array.prototype.concat.call(this.listCauHinhTaiLieuSelected, this.gridLoaiTaiLieu.dataSource);
+    for (let i = this.listCauHinhTaiLieu.length; i--;) {
+      if (this.listCauHinhTaiLieuSelected.indexOf(this.listCauHinhTaiLieu[i]) !== -1) {
+        this.listCauHinhTaiLieu.splice(i, 1);
+      }
+    }
+    this.gridCauHinhTaiLieu.refresh();
   }
 }
