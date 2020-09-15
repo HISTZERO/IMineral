a4a746c40186201af475f469c417fd109b373379
Tran Minh Tien <minhtien@vnpt.vn>
2020-09-15 17:44:34 +0700

    update cấu hình tài liệu

--- src/app/features/admin/thietlap/cauhinhtailieu/cauhinhtailieu-io/cauhinhtailieu-io.component.ts
+++ src/app/features/admin/thietlap/cauhinhtailieu/cauhinhtailieu-io/cauhinhtailieu-io.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { TranslateService } from "@ngx-translate/core";
-import { GridComponent, TextWrapSettingsModel } from "@syncfusion/ej2-angular-grids";
-import { OutputDmLoaiTaiLieuModel } from "../../../../../models/admin/danhmuc/loaitailieu.model";
-import { DmFacadeService } from "../../../../../services/admin/danhmuc/danhmuc-facade.service";
-import { ThietlapFacadeService } from "../../../../../services/admin/thietlap/thietlap-facade.service";
-import { SettingsCommon, ThietLapHeThong } from "../../../../../shared/constants/setting-common";
+import { EditService, EditSettingsModel, GridComponent, QueryCellInfoEventArgs, TextWrapSettingsModel } from "@syncfusion/ej2-angular-grids";
+import * as cloneDeep from "lodash.cloneDeep";
+
+import { OutputDmLoaiTaiLieuModel } from "src/app/models/admin/danhmuc/loaitailieu.model";
+import { DmFacadeService } from "src/app/services/admin/danhmuc/danhmuc-facade.service";
+import { ThietlapFacadeService } from "src/app/services/admin/thietlap/thietlap-facade.service";
+import { SettingsCommon, ThietLapHeThong } from "src/app/shared/constants/setting-common";
 
 @Component({
   selector: 'app-cauhinhtailieu-io',
@@ -19,6 +21,9 @@ export class CauhinhtailieuIoComponent implements OnInit {
 
   // Chứa thiết lập grid
   public settingsCommon = new SettingsCommon();
+
+  // edit setting grid
+  public editSettings: EditSettingsModel;
 
   // Chứa kiểu wrap text trên grid
   public wrapSettings: TextWrapSettingsModel;
@@ -53,9 +58,12 @@ export class CauhinhtailieuIoComponent implements OnInit {
   ) { }
 
   async ngOnInit() {
+    this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Batch' };
+
     // Setting wrap mode
     this.wrapSettings = { wrapMode: 'Both' };
     await this.getDataTranslate();
+
     // Gọi hàm lấy dữ liệu pagesize
     await this.getDataPageSize();
   }
@@ -82,6 +90,7 @@ export class CauhinhtailieuIoComponent implements OnInit {
     } else {
       this.settingsCommon.pageSettings.pageSize = 10;
     }
+
     // Gọi hàm lấy dữ liệu loại tài liệu
     await this.getAllLoaiTaiLieu();
   }
@@ -108,39 +117,67 @@ export class CauhinhtailieuIoComponent implements OnInit {
    * Hàm lấy dữ liệu khi click vào row trên grid Loại tài liệu
    */
   public rowSelectedLoaiTaiLieu() {
-    this.listLoaiTaiLieuSelected = this.gridLoaiTaiLieu.getSelectedRecords();
+    this.listLoaiTaiLieuSelected = cloneDeep(this.gridLoaiTaiLieu.getSelectedRecords());
+    if (this.listLoaiTaiLieuSelected && this.listLoaiTaiLieuSelected.length >= 1) {
+      this.disableBtnRight = false;
+    } else {
+      this.disableBtnRight = true;
+    }
   }
 
   /**
    * Hàm lấy dữ liệu khi click vào row trên grid cấu hình tài liệu
    */
   public rowSelectedCauHinhTaiLieu() {
-    this.listCauHinhTaiLieuSelected = this.gridCauHinhTaiLieu.getSelectedRecords();
+    this.listCauHinhTaiLieuSelected = cloneDeep(this.gridCauHinhTaiLieu.getSelectedRecords());
+    if (this.listCauHinhTaiLieuSelected && this.listCauHinhTaiLieuSelected.length >= 1) {
+      this.disableBtnLeft = false;
+    } else {
+      this.disableBtnLeft = true;
+    }
   }
 
   /**
    * Nút chuyển toàn bộ dữ liệu grid loại tài liệu đã chọn sang grid cấu hình tài liệu
    */
   public btnDoubleRight() {
-    this.listCauHinhTaiLieu = Array.prototype.concat.call(this.listLoaiTaiLieuSelected, this.gridCauHinhTaiLieu.dataSource);
+    this.listCauHinhTaiLieu = Array.prototype.concat.call(this.listLoaiTaiLieuSelected, this.listCauHinhTaiLieu);
     for (let i = this.listLoaitaiLieu.length; i--;) {
       if (this.listLoaiTaiLieuSelected.indexOf(this.listLoaitaiLieu[i]) !== -1) {
         this.listLoaitaiLieu.splice(i, 1);
       }
     }
     this.gridLoaiTaiLieu.refresh();
+    this.gridLoaiTaiLieu.clearSelection();
   }
 
   /**
    * Nút chuyển toàn bộ dữ liệu grid loại tài liệu đã chọn sang grid cấu hình tài liệu
    */
   public btnDoubleLeft() {
-    this.listLoaitaiLieu = Array.prototype.concat.call(this.listCauHinhTaiLieuSelected, this.gridLoaiTaiLieu.dataSource);
+    this.listLoaitaiLieu = Array.prototype.concat.call(this.listCauHinhTaiLieuSelected, this.listLoaitaiLieu);
     for (let i = this.listCauHinhTaiLieu.length; i--;) {
       if (this.listCauHinhTaiLieuSelected.indexOf(this.listCauHinhTaiLieu[i]) !== -1) {
         this.listCauHinhTaiLieu.splice(i, 1);
       }
     }
+    console.log(this.listCauHinhTaiLieu);
     this.gridCauHinhTaiLieu.refresh();
+    this.gridCauHinhTaiLieu.clearSelection();
+  }
+
+  /**
+   * Lưu dữ liệu cấu hình
+   */
+  public getAllDataCauHinh() {
+    const dataCauHinhTaiLieu: any = this.gridCauHinhTaiLieu.getBatchChanges();
+    let dataBody = {
+      list: [
+        dataCauHinhTaiLieu.changedRecords
+      ]
+    };
+    console.log(dataBody);
+    // this.thietlapFacadeService.getCauHinhTaiLieuService().updateItem(this.gridCauHinhTaiLieu.getBatchChanges());
+    console.log(this.gridCauHinhTaiLieu.getBatchChanges())
   }
 }
