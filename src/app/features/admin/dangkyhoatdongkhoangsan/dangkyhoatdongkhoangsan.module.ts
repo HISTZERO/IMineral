fab9979ec225773441b489088eb8d989bacec6fe
HISTZERO <mato9898@gmail.com>
2020-09-24 10:35:53 +0700

    Add đăng ký khai thác khoáng sản dự án

--- src/app/features/admin/dangkyhoatdongkhoangsan/dangkyhoatdongkhoangsan.module.ts
+++ src/app/features/admin/dangkyhoatdongkhoangsan/dangkyhoatdongkhoangsan.module.ts
@@ -3,65 +3,68 @@ import {
   CUSTOM_ELEMENTS_SCHEMA,
   ComponentFactoryResolver
 } from "@angular/core";
-import { CommonModule } from '@angular/common';
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { DateTimeAdapter, OWL_DATE_TIME_FORMATS, OWL_DATE_TIME_LOCALE, OwlDateTimeModule } from "ng-pick-datetime";
-import { MomentDateTimeAdapter, OwlMomentDateTimeModule, OWL_MOMENT_DATE_TIME_ADAPTER_OPTIONS } from "ng-pick-datetime-moment";
-import { MY_CUSTOM_FORMATS } from "src/app/features/admin/admin-shared.module";
-import { TranslateModule } from "@ngx-translate/core";
-import { AdminSharedModule } from "src/app/features/admin/admin-shared.module";
-import { ExComponentFactoryResolverService } from "src/app/services/utilities/ex-component-factory-resolver.service";
-import { DangkyhoatdongkhoangsanRoutingModule } from 'src/app/features/admin/dangkyhoatdongkhoangsan/dangkyhoatdongkhoangsan-routing.module';
-import { HosoListComponent } from 'src/app/features/admin/dangkyhoatdongkhoangsan/hoso/hoso-list/hoso-list.component';
-import { HosoIoComponent } from 'src/app/features/admin/dangkyhoatdongkhoangsan/hoso/hoso-io/hoso-io.component';
-import { ThamdokhoangsanIoComponent } from 'src/app/features/admin/dangkyhoatdongkhoangsan/thamdokhoangsan/thamdokhoangsan-io/thamdokhoangsan-io.component';
-import { ThamdokhoangsanListComponent } from 'src/app/features/admin/dangkyhoatdongkhoangsan/thamdokhoangsan/thamdokhoangsan-list/thamdokhoangsan-list.component';
-import { DangkyhoatdongkhoangsanComponent } from "src/app/features/admin/dangkyhoatdongkhoangsan/dangkyhoatdongkhoangsan.component";
-import { DmCanhanOptionComponent } from "src/app/features/admin/danhmuc/canhan/canhan-option/canhan-option.component";
-import { DanhmucModule } from "src/app/features/admin/danhmuc/danhmuc.module";
-import { DmTochucOptionComponent } from "src/app/features/admin/danhmuc/tochuc/tochuc-option/tochuc-option.component";
-import { HosotailieuListComponent } from './hosotailieu/hosotailieu-list/hosotailieu-list.component';
-import { HosotailieuIoComponent } from './hosotailieu/hosotailieu-io/hosotailieu-io.component';
-import { ThongtindangkyComponent } from './thamdokhoangsan/thongtindangky/thongtindangky.component';
-import { DangkythamdokhoangsanIoComponent } from './thamdokhoangsan/thongtindangky/dangkythamdokhoangsan-io/dangkythamdokhoangsan-io.component';
-import { DangkythamdogiahanIoComponent } from './thamdokhoangsan/thongtindangky/dangkythamdogiahan-io/dangkythamdogiahan-io.component';
-import { DonvihanhchinhComponent } from './thamdokhoangsan/thongtindangky/donvihanhchinh/donvihanhchinh.component';
-import { LoaikhoangsanComponent } from './thamdokhoangsan/thongtindangky/loaikhoangsan/loaikhoangsan.component';
-import { KhuvucthamdoComponent } from './thamdokhoangsan/thongtindangky/khuvucthamdo/khuvucthamdo.component';
-import { KhuvuctoadoComponent } from './thamdokhoangsan/thongtindangky/khuvuctoado/khuvuctoado.component';
-import { DonvihanhchinhIoComponent } from './thamdokhoangsan/thongtindangky/donvihanhchinh/donvihanhchinh-io/donvihanhchinh-io.component';
-import { DonvihanhchinhListComponent } from './thamdokhoangsan/thongtindangky/donvihanhchinh/donvihanhchinh-list/donvihanhchinh-list.component';
-import { LoaikhoangsanIoComponent } from './thamdokhoangsan/thongtindangky/loaikhoangsan/loaikhoangsan-io/loaikhoangsan-io.component';
-import { LoaikhoangsanListComponent } from './thamdokhoangsan/thongtindangky/loaikhoangsan/loaikhoangsan-list/loaikhoangsan-list.component';
-import { KhuvucthamdoIoComponent } from './thamdokhoangsan/thongtindangky/khuvucthamdo/khuvucthamdo-io/khuvucthamdo-io.component';
-import { KhuvucthamdoListComponent } from './thamdokhoangsan/thongtindangky/khuvucthamdo/khuvucthamdo-list/khuvucthamdo-list.component';
-import { KhuvuctoadoIoComponent } from './thamdokhoangsan/thongtindangky/khuvuctoado/khuvuctoado-io/khuvuctoado-io.component';
-import { KhuvuctoadoListComponent } from './thamdokhoangsan/thongtindangky/khuvuctoado/khuvuctoado-list/khuvuctoado-list.component';
-import { CongtrinhthamdoComponent } from './thamdokhoangsan/thongtindangky/congtrinhthamdo/congtrinhthamdo.component';
-import { CongtrinhthamdoIoComponent } from './thamdokhoangsan/thongtindangky/congtrinhthamdo/congtrinhthamdo-io/congtrinhthamdo-io.component';
-import { CongtrinhthamdoListComponent } from './thamdokhoangsan/thongtindangky/congtrinhthamdo/congtrinhthamdo-list/congtrinhthamdo-list.component';
-import { KhaithackhoangsanListComponent } from './khaithackhoangsan/khaithackhoangsan-list/khaithackhoangsan-list.component';
-import { KhaithackhoangsanIoComponent } from './khaithackhoangsan/khaithackhoangsan-io/khaithackhoangsan-io.component';
-import { KtksThongtindangkyComponent } from "./khaithackhoangsan/ktks-thongtindangky/ktks-thongtindangky.component";
-import { DangkykhaithacgiahanIoComponent } from "./khaithackhoangsan/ktks-thongtindangky/dangkykhaithacgiahan-io/dangkykhaithacgiahan-io.component";
-import { DangkykhaithackhoangsanIoComponent } from "./khaithackhoangsan/ktks-thongtindangky/dangkykhaithackhoangsan-io/dangkykhaithackhoangsan-io.component";
-import { TanthukhoangsanListComponent } from './tanthukhoangsan/tanthukhoangsan-list/tanthukhoangsan-list.component';
-import { TanthukhoangsanIoComponent } from './tanthukhoangsan/tanthukhoangsan-io/tanthukhoangsan-io.component';
-import { TralaigiayphepListComponent } from './tralaigiayphep/tralaigiayphep-list/tralaigiayphep-list.component';
-import { TralaigiayphepIoComponent } from './tralaigiayphep/tralaigiayphep-io/tralaigiayphep-io.component';
-import { DongcuamoListComponent } from './dongcuamo/dongcuamo-list/dongcuamo-list.component';
-import { DongcuamoIoComponent } from './dongcuamo/dongcuamo-io/dongcuamo-io.component';
-import { ChuyennhuongquyenListComponent } from './chuyennhuongquyen/chuyennhuongquyen-list/chuyennhuongquyen-list.component';
-import { ChuyennhuongquyenIoComponent } from './chuyennhuongquyen/chuyennhuongquyen-io/chuyennhuongquyen-io.component';
+import {CommonModule} from '@angular/common';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {DateTimeAdapter, OWL_DATE_TIME_FORMATS, OWL_DATE_TIME_LOCALE, OwlDateTimeModule} from "ng-pick-datetime";
+import {
+  MomentDateTimeAdapter,
+  OwlMomentDateTimeModule,
+  OWL_MOMENT_DATE_TIME_ADAPTER_OPTIONS
+} from "ng-pick-datetime-moment";
+import {MY_CUSTOM_FORMATS} from "src/app/features/admin/admin-shared.module";
+import {TranslateModule} from "@ngx-translate/core";
+import {AdminSharedModule} from "src/app/features/admin/admin-shared.module";
+import {ExComponentFactoryResolverService} from "src/app/services/utilities/ex-component-factory-resolver.service";
+import {DangkyhoatdongkhoangsanRoutingModule} from 'src/app/features/admin/dangkyhoatdongkhoangsan/dangkyhoatdongkhoangsan-routing.module';
+import {HosoListComponent} from 'src/app/features/admin/dangkyhoatdongkhoangsan/hoso/hoso-list/hoso-list.component';
+import {HosoIoComponent} from 'src/app/features/admin/dangkyhoatdongkhoangsan/hoso/hoso-io/hoso-io.component';
+import {ThamdokhoangsanIoComponent} from 'src/app/features/admin/dangkyhoatdongkhoangsan/thamdokhoangsan/thamdokhoangsan-io/thamdokhoangsan-io.component';
+import {ThamdokhoangsanListComponent} from 'src/app/features/admin/dangkyhoatdongkhoangsan/thamdokhoangsan/thamdokhoangsan-list/thamdokhoangsan-list.component';
+import {DangkyhoatdongkhoangsanComponent} from "src/app/features/admin/dangkyhoatdongkhoangsan/dangkyhoatdongkhoangsan.component";
+import {DmCanhanOptionComponent} from "src/app/features/admin/danhmuc/canhan/canhan-option/canhan-option.component";
+import {DanhmucModule} from "src/app/features/admin/danhmuc/danhmuc.module";
+import {DmTochucOptionComponent} from "src/app/features/admin/danhmuc/tochuc/tochuc-option/tochuc-option.component";
+import {HosotailieuListComponent} from './hosotailieu/hosotailieu-list/hosotailieu-list.component';
+import {HosotailieuIoComponent} from './hosotailieu/hosotailieu-io/hosotailieu-io.component';
+import {ThongtindangkyComponent} from './thamdokhoangsan/thongtindangky/thongtindangky.component';
+import {DangkythamdokhoangsanIoComponent} from './thamdokhoangsan/thongtindangky/dangkythamdokhoangsan-io/dangkythamdokhoangsan-io.component';
+import {DangkythamdogiahanIoComponent} from './thamdokhoangsan/thongtindangky/dangkythamdogiahan-io/dangkythamdogiahan-io.component';
+import {DonvihanhchinhComponent} from './thamdokhoangsan/thongtindangky/donvihanhchinh/donvihanhchinh.component';
+import {LoaikhoangsanComponent} from './thamdokhoangsan/thongtindangky/loaikhoangsan/loaikhoangsan.component';
+import {KhuvucthamdoComponent} from './thamdokhoangsan/thongtindangky/khuvucthamdo/khuvucthamdo.component';
+import {KhuvuctoadoComponent} from './thamdokhoangsan/thongtindangky/khuvuctoado/khuvuctoado.component';
+import {DonvihanhchinhIoComponent} from './thamdokhoangsan/thongtindangky/donvihanhchinh/donvihanhchinh-io/donvihanhchinh-io.component';
+import {DonvihanhchinhListComponent} from './thamdokhoangsan/thongtindangky/donvihanhchinh/donvihanhchinh-list/donvihanhchinh-list.component';
+import {LoaikhoangsanIoComponent} from './thamdokhoangsan/thongtindangky/loaikhoangsan/loaikhoangsan-io/loaikhoangsan-io.component';
+import {LoaikhoangsanListComponent} from './thamdokhoangsan/thongtindangky/loaikhoangsan/loaikhoangsan-list/loaikhoangsan-list.component';
+import {KhuvucthamdoIoComponent} from './thamdokhoangsan/thongtindangky/khuvucthamdo/khuvucthamdo-io/khuvucthamdo-io.component';
+import {KhuvucthamdoListComponent} from './thamdokhoangsan/thongtindangky/khuvucthamdo/khuvucthamdo-list/khuvucthamdo-list.component';
+import {KhuvuctoadoIoComponent} from './thamdokhoangsan/thongtindangky/khuvuctoado/khuvuctoado-io/khuvuctoado-io.component';
+import {KhuvuctoadoListComponent} from './thamdokhoangsan/thongtindangky/khuvuctoado/khuvuctoado-list/khuvuctoado-list.component';
+import {CongtrinhthamdoComponent} from './thamdokhoangsan/thongtindangky/congtrinhthamdo/congtrinhthamdo.component';
+import {CongtrinhthamdoIoComponent} from './thamdokhoangsan/thongtindangky/congtrinhthamdo/congtrinhthamdo-io/congtrinhthamdo-io.component';
+import {CongtrinhthamdoListComponent} from './thamdokhoangsan/thongtindangky/congtrinhthamdo/congtrinhthamdo-list/congtrinhthamdo-list.component';
+import {KhaithackhoangsanListComponent} from './khaithackhoangsan/khaithackhoangsan-list/khaithackhoangsan-list.component';
+import {KhaithackhoangsanIoComponent} from './khaithackhoangsan/khaithackhoangsan-io/khaithackhoangsan-io.component';
+import {KtksThongtindangkyComponent} from "./khaithackhoangsan/ktks-thongtindangky/ktks-thongtindangky.component";
+import {DangkykhaithacgiahanIoComponent} from "./khaithackhoangsan/ktks-thongtindangky/dangkykhaithacgiahan-io/dangkykhaithacgiahan-io.component";
+import {DangkykhaithackhoangsanIoComponent} from "./khaithackhoangsan/ktks-thongtindangky/dangkykhaithackhoangsan-io/dangkykhaithackhoangsan-io.component";
+import {TanthukhoangsanListComponent} from './tanthukhoangsan/tanthukhoangsan-list/tanthukhoangsan-list.component';
+import {TanthukhoangsanIoComponent} from './tanthukhoangsan/tanthukhoangsan-io/tanthukhoangsan-io.component';
+import {TralaigiayphepListComponent} from './tralaigiayphep/tralaigiayphep-list/tralaigiayphep-list.component';
+import {TralaigiayphepIoComponent} from './tralaigiayphep/tralaigiayphep-io/tralaigiayphep-io.component';
+import {DongcuamoListComponent} from './dongcuamo/dongcuamo-list/dongcuamo-list.component';
+import {DongcuamoIoComponent} from './dongcuamo/dongcuamo-io/dongcuamo-io.component';
+import {ChuyennhuongquyenListComponent} from './chuyennhuongquyen/chuyennhuongquyen-list/chuyennhuongquyen-list.component';
+import {ChuyennhuongquyenIoComponent} from './chuyennhuongquyen/chuyennhuongquyen-io/chuyennhuongquyen-io.component';
 import {PheduyettruluongkhoangsanListComponent} from "./pheduyettruluongkhoangsan/pheduyettruluongkhoangsan-list/pheduyettruluongkhoangsan-list.component";
 import {PheduyettruluongkhoangsanIoComponent} from "./pheduyettruluongkhoangsan/pheduyettruluongkhoangsan-io/pheduyettruluongkhoangsan-io.component";
 import {DaugiaquyenListComponent} from "./daugiaquyen/daugiaquyen-list/daugiaquyen-list.component";
 import {DaugiaquyenIoComponent} from "./daugiaquyen/daugiaquyen-io/daugiaquyen-io.component";
-import { DangkykhaithaccaisoiIoComponent } from './khaithackhoangsan/ktks-thongtindangky/dangkykhaithaccaisoi-io/dangkykhaithaccaisoi-io.component';
-import { DangkykhaithacdieuchinhIoComponent } from './khaithackhoangsan/ktks-thongtindangky/dangkykhaithacdieuchinh-io/dangkykhaithacdieuchinh-io.component';
-import { DangkykhaithacvlxdIoComponent } from './khaithackhoangsan/ktks-thongtindangky/dangkykhaithacvlxd-io/dangkykhaithacvlxd-io.component';
-import { DangkykhaithackhoangsanduanIoComponent } from './khaithackhoangsan/ktks-thongtindangky/dangkykhaithackhoangsanduan-io/dangkykhaithackhoangsanduan-io.component';
-
+import {DangkykhaithaccaisoiIoComponent} from './khaithackhoangsan/ktks-thongtindangky/dangkykhaithaccaisoi-io/dangkykhaithaccaisoi-io.component';
+import {DangkykhaithacdieuchinhIoComponent} from './khaithackhoangsan/ktks-thongtindangky/dangkykhaithacdieuchinh-io/dangkykhaithacdieuchinh-io.component';
+import {DangkykhaithacvlxdIoComponent} from './khaithackhoangsan/ktks-thongtindangky/dangkykhaithacvlxd-io/dangkykhaithacvlxd-io.component';
+import {DangkykhaithackhoangsanduanIoComponent} from './khaithackhoangsan/ktks-thongtindangky/dangkykhaithackhoangsanduan-io/dangkykhaithackhoangsanduan-io.component';
 
 
 @NgModule({
@@ -104,7 +107,7 @@ import { DangkykhaithackhoangsanduanIoComponent } from './khaithackhoangsan/ktks
     DongcuamoIoComponent,
     ChuyennhuongquyenListComponent,
     ChuyennhuongquyenIoComponent,
-     PheduyettruluongkhoangsanIoComponent,
+    PheduyettruluongkhoangsanIoComponent,
     PheduyettruluongkhoangsanListComponent,
     DaugiaquyenIoComponent,
     DaugiaquyenListComponent,
@@ -145,10 +148,10 @@ import { DangkykhaithackhoangsanduanIoComponent } from './khaithackhoangsan/ktks
     DangkykhaithackhoangsanduanIoComponent
   ],
   providers: [
-    { provide: OWL_MOMENT_DATE_TIME_ADAPTER_OPTIONS, useValue: { useUtc: true } },
+    {provide: OWL_MOMENT_DATE_TIME_ADAPTER_OPTIONS, useValue: {useUtc: true}},
     // { provide: OWL_DATE_TIME_LOCALE, useValue: 'vi' },
     // { provide: DateTimeAdapter, useClass: MomentDateTimeAdapter, deps: [OWL_DATE_TIME_LOCALE] },
-    { provide: OWL_DATE_TIME_FORMATS, useValue: MY_CUSTOM_FORMATS },
+    {provide: OWL_DATE_TIME_FORMATS, useValue: MY_CUSTOM_FORMATS},
   ]
 })
 export class DangkyhoatdongkhoangsanModule {
--- src/app/features/admin/dangkyhoatdongkhoangsan/khaithackhoangsan/ktks-thongtindangky/dangkykhaithackhoangsanduan-io/dangkykhaithackhoangsanduan-io.component.ts
+++ src/app/features/admin/dangkyhoatdongkhoangsan/khaithackhoangsan/ktks-thongtindangky/dangkykhaithackhoangsanduan-io/dangkykhaithackhoangsanduan-io.component.ts
@@ -1,4 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, ComponentFactoryResolver, EventEmitter, Output} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {ActivatedRoute} from '@angular/router';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {HttpErrorResponse} from '@angular/common/http';
+
+import {DmFacadeService} from "src/app/services/admin/danhmuc/danhmuc-facade.service";
+import {DangKyHoatDongKhoangSanFacadeService} from 'src/app/services/admin/dangkyhoatdongkhoangsan/dangkyhoatdongkhoangsan-facade.service';
+import {DangKhoangSanEnum, DangKyKhaiThacKsActionEnum} from 'src/app/shared/constants/enum';
+import {CommonServiceShared} from 'src/app/services/utilities/common-service';
+import {validationAllErrorMessagesService} from "src/app/services/utilities/validatorService";
+import {OutputDmHeQuyChieuModel} from 'src/app/models/admin/danhmuc/hequychieu.model';
+import {
+  DangKhoangSan,
+  DonViCongSuat,
+  DonViDienTich,
+  DonViDoSau,
+  DonViThoiHan,
+  DonViTruLuong
+} from 'src/app/shared/constants/common-constants';
+import {OutputDkKhaiThacKhoangSanDuAnModel} from "../../../../../../models/admin/dangkyhoatdongkhoangsan/dkkhaithackhoangsanduan.model";
 
 @Component({
   selector: 'app-dangkykhaithackhoangsanduan-io',
@@ -7,9 +27,349 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DangkykhaithackhoangsanduanIoComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  // tslint:disable-next-line: no-output-rename
+  @Output("selectCurrentFormStateEvent") selectCurrentFormStateEvent: EventEmitter<number> = new EventEmitter();
+  // tslint:disable-next-line: no-output-rename
+  @Output("selectIdDangKyKhaiThacGiaHanEvent") selectIdDangKyKhaiThacGiaHanEvent: EventEmitter<string> = new EventEmitter();
+  // tslint:disable-next-line: no-input-rename
+  @Input("allowAutoInit") allowAutoInit = true;
+  // Nhóm loại cấp phép
+  // tslint:disable-next-line: no-input-rename
+  @Input("nhomLoaiCapPhep") nhomLoaiCapPhep;
+  // Chứa dữ liệu Form
+  public dangKyKhaiThacKhoangSanDuAnIOForm: FormGroup;
+  // Chứa dữ liệu translate
+  public dataTranslate: any;
+  // Chứa danh sách Hệ quy chiếu
+  public allHeQuyChieu: OutputDmHeQuyChieuModel[];
+  public HeQuyChieuFilters: OutputDmHeQuyChieuModel[];
+  // Dạng khoáng sản
+  public dangKhoangSanList = DangKhoangSan;
+  // chứa dữ liệu Id Hồ sơ
+  public idhoso: string;
+  // chứa dữ liệu đăng ký thăm dò
+  private dangKyKhaiThacKhoangSanDuAn: any;
+  // Action thao tác dữ liệu
+  public currentAction: number;
+  // Action đăng ký thăm dò
+  public ActionType = DangKyKhaiThacKsActionEnum;
+  // disable delete button
+  public disabledDeleteButton = false;
+  // lưu dữ liệu đơn vị diện tích
+  public donViDienTichList = DonViDienTich;
+  // Chứa đon vị trữ lượng
+  public donViTruLuongList = DonViTruLuong;
+  // Lưu trữ đơn vị thời hạn
+  public donViThoiHanList = DonViThoiHan;
+  // lưu dữ liệu đơn vị diện tích
+  public donViDoSauList = DonViDoSau;
+  // Chứa đơn vị công suất
+  public donViCongSuat = DonViCongSuat;
+  // error message
+  validationErrorMessages = {};
+
+  // form errors
+  formErrors = {
+    tenduan: "",
+    soquyetdinh: "",
+    ngaykyquyetdinh: "",
+    coquanpheduyet: "",
+    diadiem: "",
+    dientichkhaithac: "",
+    truluongdiachat: "",
+    truluongkhaithac: "",
+    thoihankhaithac: "",
+    phuongphapkhaithac: "",
+    congsuatkhaithac: "",
+    mucsaukhaithactu: "",
+    mucsaukhaithacden: "",
+    dangkhoangsan: "",
+    mucdichsudungkhoangsan: "",
+    donvitruluong: "",
+    donvicongsuat: "",
+    donvidientich: "",
+    donvithoihan: "",
+    donvichieusau: "",
+    hequychieu: ""
+  };
+
+  constructor(
+    private translate: TranslateService,
+    private formBuilder: FormBuilder,
+    private dmFacadeService: DmFacadeService,
+    private dangKyHoatDongKhoangSanFacadeService: DangKyHoatDongKhoangSanFacadeService,
+    public commonService: CommonServiceShared,
+    private activatedRoute: ActivatedRoute,
+    public cfr: ComponentFactoryResolver
+  ) {
   }
+
+  async ngOnInit() {
+    // Khởi tạo form
+    this.formInit();
+    // Lấy dữ liệu translate
+    await this.getDataTranslate();
+
+    if (this.allowAutoInit) {
+      await this.manualDataInit();
+    }
+  }
+
+  /**
+   * Khởi tạo form
+   */
+  async manualDataInit() {
+    this.activatedRoute.queryParamMap.subscribe((param: any) => {
+      if (param && param.params && param.params.idhoso) {
+        this.idhoso = param.params.idhoso;
+      }
+    });
+
+    if (this.idhoso !== null && this.idhoso !== undefined) {
+      this.dangKyKhaiThacKhoangSanDuAn = await this.getDangKyKhaiThacKhoangSanDuAnByIdHoSo(this.idhoso);
+
+      if (this.dangKyKhaiThacKhoangSanDuAn) {
+        this.currentAction = DangKyKhaiThacKsActionEnum.Edit;
+        this.selectIddangKyKhaiThacKhoangSanDuAn();
+        this.selectCurrentFormState();
+      } else {
+        this.currentAction = DangKyKhaiThacKsActionEnum.Add;
+        this.selectCurrentFormState();
+      }
+    } else {
+      this.currentAction = DangKyKhaiThacKsActionEnum.None;
+      return;
+    }
+
+    // Lấy dữ liệu hệ quy chiếu
+    await this.geAllHeQuyChieu();
+    // Khởi tạo dữ liệu form trong trường hợp sửa dữ liệu Hồ Sơ
+    await this.formOnEdit(this.dangKyKhaiThacKhoangSanDuAn);
+
+    return true;
+  }
+
+  /**
+   * Hàm khởi tạo form
+   */
+  private formInit() {
+    this.dangKyKhaiThacKhoangSanDuAnIOForm = this.formBuilder.group({
+      tenduan: [""],
+      soquyetdinh: [""],
+      ngaykyquyetdinh: [""],
+      coquanpheduyet: [""],
+      diadiem: [""],
+      dientichkhaithac: [""],
+      truluongdiachat: [""],
+      truluongkhaithac: [""],
+      thoihankhaithac: [""],
+      phuongphapkhaithac: [""],
+      congsuatkhaithac: [""],
+      mucsaukhaithactu: [""],
+      mucsaukhaithacden: [""],
+      dangkhoangsan: [""],
+      mucdichsudungkhoangsan: [""],
+      donvitruluong: [""],
+      donvicongsuat: [""],
+      donvidientich: [""],
+      donvithoihan: [""],
+      donvichieusau: [""],
+      hequychieu: [""],
+    });
+  }
+
+  /**
+   * hàm lấy dữ liệu translate
+   */
+  async getDataTranslate() {
+    // Lấy ra biến translate của hệ thống
+    this.dataTranslate = await this.translate
+      .getTranslation(this.translate.getDefaultLang())
+      .toPromise();
+    // Hàm set validation cho form
+    await this.setValidation();
+  }
+
+  /**
+   * hàm set value cho form
+   */
+  private async formOnEdit(item: OutputDkKhaiThacKhoangSanDuAnModel) {
+    if (this.currentAction === DangKyKhaiThacKsActionEnum.Edit && item) {
+      this.dangKyKhaiThacKhoangSanDuAnIOForm.setValue({
+        tenduan: item.tenduan,
+        soquyetdinh: item.soquyetdinh,
+        ngaykyquyetdinh: item.ngaykyquyetdinh,
+        coquanpheduyet: item.coquanpheduyet,
+        diadiem: item.diadiem,
+        dientichkhaithac: item.dientichkhaithac,
+        truluongdiachat: item.truluongdiachat,
+        truluongkhaithac: item.truluongkhaithac,
+        thoihankhaithac: item.thoihankhaithac,
+        phuongphapkhaithac: item.phuongphapkhaithac,
+        congsuatkhaithac: item.congsuatkhaithac,
+        mucsaukhaithactu: item.mucsaukhaithactu,
+        mucsaukhaithacden: item.mucsaukhaithacden,
+        dangkhoangsan: item.dangkhoangsan,
+        mucdichsudungkhoangsan: item.mucdichsudungkhoangsan,
+        donvitruluong: item.donvitruluong,
+        donvicongsuat: item.donvicongsuat,
+        donvidientich: item.donvidientich,
+        donvithoihan: item.donvithoihan,
+        donvichieusau: item.donvichieusau,
+        hequychieu: item.hequychieu,
+      });
+    }
+  }
+
+  /**
+   * Hàm set validate
+   */
+  private setValidation() {
+    this.validationErrorMessages = {};
+  }
+
+  /**
+   * Hàm lấy danh sách Lĩnh Vực
+   */
+  async geAllHeQuyChieu() {
+    const allHeQuyChieuData: any = await this.dmFacadeService
+      .getDmHeQuyChieuService()
+      .getFetchAll({PageNumber: 1, PageSize: -1});
+    this.allHeQuyChieu = allHeQuyChieuData.items;
+    this.HeQuyChieuFilters = allHeQuyChieuData.items;
+  }
+
+  /**
+   * Lấy dữ liệu hồ sơ theo IdHoSo
+   * @param idHoSo
+   */
+  private async getDangKyKhaiThacKhoangSanDuAnByIdHoSo(idHoSo: string) {
+    const dkKhaiThacKhoangSanDuAnService = this.dangKyHoatDongKhoangSanFacadeService.getDangKyKhaiThacKhoangSanDuAnService();
+    const dangKyItem = await dkKhaiThacKhoangSanDuAnService.getFetchAll({Idhoso: idHoSo});
+    return dangKyItem;
+  }
+
+  async saveItemdangKyKhaiThacKhoangSanDuAn() {
+    this.logAllValidationErrorMessages();
+
+    if (!this.dangKyKhaiThacKhoangSanDuAnIOForm.valid) {
+      return;
+    }
+
+    // Gán dữ liệu input vào model
+    const dkKhaiThacKhoangSanDuAnService = this.dangKyHoatDongKhoangSanFacadeService.getDangKyKhaiThacKhoangSanDuAnService();
+    const inputModel = this.dangKyKhaiThacKhoangSanDuAnIOForm.value;
+    inputModel.idhoso = this.idhoso;
+    if (this.currentAction === DangKyKhaiThacKsActionEnum.Add) {
+      dkKhaiThacKhoangSanDuAnService.addItem(inputModel).subscribe(
+        async (res) => {
+          this.dangKyKhaiThacKhoangSanDuAn = inputModel;
+          this.dangKyKhaiThacKhoangSanDuAn.iddangkykhaithac = res.iddangkykhaithac;
+          this.currentAction = DangKyKhaiThacKsActionEnum.Edit;
+          this.selectCurrentFormState();
+          this.selectIddangKyKhaiThacKhoangSanDuAn();
+        },
+        (error: HttpErrorResponse) => {
+          this.commonService.showDialogWarning(error.error.errors);
+        },
+        () =>
+          this.commonService.showeNotiResult(
+            this.dataTranslate.COMMON.default.successAdd,
+            2000
+          )
+      );
+    } else if (this.currentAction === DangKyKhaiThacKsActionEnum.Edit) {
+      inputModel.iddangkykhaithac = this.dangKyKhaiThacKhoangSanDuAn.iddangkykhaithac;
+      dkKhaiThacKhoangSanDuAnService.updateItem(inputModel).subscribe(
+        async (res) => {
+          this.dangKyKhaiThacKhoangSanDuAn = inputModel;
+          this.currentAction = DangKyKhaiThacKsActionEnum.Edit;
+          this.selectCurrentFormState();
+        },
+        (error: HttpErrorResponse) => {
+          this.commonService.showDialogWarning(error.error.errors);
+        },
+        () =>
+          this.commonService.showeNotiResult(
+            this.dataTranslate.COMMON.default.successEdit,
+            2000
+          )
+      );
+    }
+  }
+
+  /**
+   * hàm kiểm tra validation form
+   */
+  private logAllValidationErrorMessages() {
+    validationAllErrorMessagesService(
+      this.dangKyKhaiThacKhoangSanDuAnIOForm,
+      this.validationErrorMessages,
+      this.formErrors
+    );
+  }
+
+  /**
+   * select inserted state of form
+   */
+  private selectCurrentFormState() {
+    if (this.currentAction === DangKyKhaiThacKsActionEnum.Edit) {
+      this.disabledDeleteButton = false;
+    } else {
+      this.disabledDeleteButton = true;
+    }
+
+    this.selectCurrentFormStateEvent.emit(this.currentAction);
+  }
+
+  /**
+   * lấy thông tin id hồ sơ sau khi thêm mới một hồ sơ
+   */
+  private selectIddangKyKhaiThacKhoangSanDuAn() {
+    this.selectIdDangKyKhaiThacGiaHanEvent.emit(this.dangKyKhaiThacKhoangSanDuAn.iddangkykhaithac);
+  }
+
+  /**
+   *
+   */
+  deleteItemdangKyKhaiThacKhoangSanDuAn() {
+    const dialogRef = this.commonService.confirmDeleteDiaLogService(
+      "",
+      ""
+    );
+    dialogRef.afterClosed().subscribe(async (result) => {
+      if (result === "confirm") {
+        await this.dangKyHoatDongKhoangSanFacadeService
+          .getDangKyKhaiThacKhoangSanDuAnService()
+          .deleteItem({iddangkykhaithac: this.dangKyKhaiThacKhoangSanDuAn.iddangkykhaithac})
+          .subscribe(
+            () => {
+              this.dangKyKhaiThacKhoangSanDuAn = null;
+              this.currentAction = DangKyKhaiThacKsActionEnum.Add;
+              this.onFormReset();
+              this.selectCurrentFormState();
+            },
+            (error: HttpErrorResponse) => {
+              this.commonService.showDialogWarning(error.error.errors);
+            },
+            () =>
+              this.commonService.showeNotiResult(
+                this.dataTranslate.COMMON.default.successDelete,
+                2000
+              )
+          );
+      }
+    });
+  }
+
+  /**
+   * Hàm reset form, gọi khi nhấn nút reset dữ liệu
+   */
+  onFormReset() {
+    // Hàm .reset sẽ xóa trắng mọi control trên form
+    this.dangKyKhaiThacKhoangSanDuAnIOForm.reset();
+  }
+
 
 }
