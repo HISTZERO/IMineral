88b71ce5d5dfc14816a5fd16a29b8141493ea4d7
Nguyen Ngoc Minh <minhnn1@vnpt.vn>
2020-10-13 10:24:37 +0700

    commit commit code     - commit  code tính tiền cấp quyền

---
+++ src/app/models/admin/tinhtiencapquyen/tttinhtiencapquyen.model.ts
@@ -0,0 +1,28 @@
+export class OutputTtTinhTienCapQuyenModel {
+  idtinhtiencapquyen: string;
+  giatinhtienlandau: string;
+  tongtienphainop: number;
+  solannop: number;
+  sotienhangnam: number;
+  sotienlandau: number;
+  namnoplandau: number;
+  idgiayphep: string;
+  sogiayphep: string;
+  ngaycapphep: Date;
+  coquancapphep: string;
+  sogiayphepls: string;
+  siteid: string;
+}
+
+export class InputTtTinhTienCapQuyenModel {
+  idtinhtiencapquyen: string;
+  giatinhtienlandau: string;
+  tongtienphainop: number;
+  solannop: number;
+  sotienhangnam: number;
+  sotienlandau: number;
+  namnoplandau: number;
+  idgiayphep: string;
+  siteid: string;
+}
+
---
+++ src/app/services/admin/tinhtiencapquyen/tinhtiencapquyen.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { RepositoryEloquentService } from "src/app/services/data/baserepository.service";
+import { environment } from "src/environments/environment";
+import { ServiceName } from "src/app/shared/constants/service-name";
+import { InputTtTinhTienCapQuyenModel, OutputTtTinhTienCapQuyenModel } from 'src/app/models/admin/tinhtiencapquyen/tttinhtiencapquyen.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TinhTienCapQuyenService extends RepositoryEloquentService {
+
+  constructor(public httpClient: HttpClient) {
+    super();
+    this.setServiceInfo({
+      httpClient,
+      inputModelName: new InputTtTinhTienCapQuyenModel(),
+      outputModelName: new OutputTtTinhTienCapQuyenModel(),
+      apiUrl: environment.apiIMineral + ServiceName.TTTINHTIENCAPQUYEN
+    });
+  }
+
+  public checkBeDeleted(id: string) {
+    return "ok";
+  }
+
+  public getTinhTienCapQuyenByIdGiayPhep(idGiayPhep: any) {
+    try {
+      return this.httpClient.get(`${this.apiUrl}?idgiayphep=${idGiayPhep}`, {
+        headers: this.headers,
+      });
+    } catch (error) {
+
+    }
+  }
+}
